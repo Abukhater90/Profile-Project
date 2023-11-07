c4ae6409054f730a883400c601f88b153f21f763
Alie <abu.khater90@gmail.com>
2023-11-07 14:30:24 +0200

    photo

--- src/components/3-Main/Main.jsx
+++ src/components/3-Main/Main.jsx
@@ -1,9 +1,10 @@
+
 import "./Main.css";
 
 const Main = () => {
   return (
     <main className="flex">
-      <section className="flex left-section ">
+      <section className="flex left-section">
         <button className="active">All Projects</button>
         <button>HTML & CSS</button>
         <button>Javascript</button>
@@ -11,43 +12,33 @@ const Main = () => {
         <button>Node & Express</button>
       </section>
 
-      {["aa", "bb", "cc" , 1 , 2 ].map((item) => {
-        return (
-          <section className="flex right-section ">
-
-            <article key={item} className="card ">
-
-
-              <img width= {266} 
-                src="https://www.sliderrevolution.com/wp-content/uploads/2021/10/cofe-header.png" />
-
-              <div style={{ width: "266px" }} className="box ">
+      <section className="flex right-section">
+        {["aa", "bb", "cc", 1, 2 , 3].map((item, index) => {
+          return (
+            <article key={index} className="card">
+              <img width={266} src="https://www.sliderrevolution.com/wp-content/uploads/2021/10/cofe-header.png" alt={`Project ${index}`} />
+              <div style={{ width: "266px" }} className="box">
                 <h1 className="title">Landing Page</h1>
                 <p className="sub-title">
                   Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                   Maiores unde qui incidunt deserunt non dolorum ratione
                   voluptate, illo quos tempore!
                 </p>
-
                 <div className="flex icons">
                   <div style={{ gap: "11px" }} className="flex">
                     <div className="icon-link"></div>
                     <div className="icon-github"></div>
                   </div>
-
-                  <a className="link flex" href="">
+                  <a className="link flex" href="https://www.example.com"> {/* Add a proper href */}
                     More
-                    <span
-                      style={{ alignSelf: "end" }}
-                      className="icon-arrow-right2"
-                    ></span>
+                    <span style={{ alignSelf: "end" }} className="icon-arrow-right2"></span>
                   </a>
                 </div>
               </div>
             </article>
-          </section>
-        );
-      })}
+          );
+        })}
+      </section>
     </main>
   );
 };
